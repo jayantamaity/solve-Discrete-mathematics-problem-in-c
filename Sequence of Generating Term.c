69c5d7419d6599570b357984748d93774f0c821f
Jayanta Kumar Maity <jayantamaity.icvp@gmail.com>
2018-02-14 21:11:04 +0530

    gene

---
+++ Sequence of Generating Term.c
@@ -0,0 +1,17 @@
+// Sequence of Generating Term
+#include<stdio.h>
+#include<math.h>
+void main()
+{  
+int i,n;
+float x,sum=0;
+
+printf("1+x+x^2+......+x^n");
+printf("\nEnter the value of x and n:");
+scanf("%f%d",&x,&n);
+for(i=1;i<=n;++i)
+sum+=pow(x,i);
+sum++;
+printf("\nSum=%f",sum);
+ 
+ }
